37b8871cd7cc553776fda12e9f924e760f693bf3
mashabodowskaya <mashabodovskaya@gmail.com>
2024-07-04 17:08:38 +0200

    I created a complex front for my homepage using html and css + js.

--- onlinestore/settings.py
+++ onlinestore/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'onlinestore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,6 +118,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'onlineshop/static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
