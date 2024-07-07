030980dfbd0b46f731fd0914aebeb5bacf81c981
mashabodowskaya <mashabodovskaya@gmail.com>
2024-07-08 00:54:13 +0200

    Created Model for Posts, adjusted the admin, set up views

--- onlineshop/views.py
+++ onlineshop/views.py
@@ -2,21 +2,7 @@ from time import timezone
 from django.utils import timezone
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [{
-    'author': 'Julianna Daffner',
-    'title': 'Post 1',
-    'content': 'welcome to my blog',
-    'date': timezone.now()
-},
-
-    {
-        'author': 'Jane Doe',
-        'title': 'Post 3',
-        'content': 'i am dead',
-        'date': '2nd October, 3933',
-    }
-]
+from .models import Post
 
 
 def show_home(request):
@@ -29,6 +15,6 @@ def about(request):
 
 def clothes(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
-    return render(request, 'onlineshop/clothes.html', {'posts': posts})
+    return render(request, 'onlineshop/clothes.html', context)
