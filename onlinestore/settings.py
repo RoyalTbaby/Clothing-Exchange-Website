d62c775bdebbbe414cbf673e058634383674e447
mashabodowskaya <mashabodovskaya@gmail.com>
2024-07-09 02:59:43 +0200

    Register HTML is almost completed

--- onlinestore/settings.py
+++ onlinestore/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'onlineshop.apps.OnlineshopConfig',
     'users.apps.UsersConfig',
+    'crispy_forms',
+    'crispy_bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +127,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'onlineshop/static')]
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
