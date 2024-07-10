3b665b291acdd6de812aa96ee7a781ab9dcfff94
mashabodowskaya <mashabodovskaya@gmail.com>
2024-07-10 19:13:15 +0200

    Starting to work on the CRUD for my posts in the clothes view. Adding LoginRequiredMixin and UserPassesTestMixin to allow user leave posts only once logged in and do not interfere in editing other users' posts.

--- onlineshop/urls.py
+++ onlineshop/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from . import views
-from .views import PostListView, PostDetailView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView
 
 
 urlpatterns = [
     path('', views.show_home, name='show_home'),
     path('community/', views.about, name='community'),
     path('clothes/', PostListView.as_view(), name='clothes'),
+    path('post/new', PostCreateView.as_view(), name='post_new'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('post/<int:pk>/update', PostUpdateView.as_view(), name='post_update'),
     ]
--- onlineshop/views.py
+++ onlineshop/views.py
@@ -2,8 +2,8 @@ from django.utils import timezone
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
 def show_home(request):
@@ -14,13 +14,44 @@ def about(request):
     return render(request, 'onlineshop/community.html')
 
 
-class PostListView(ListView):
+class PostListView(LoginRequiredMixin, ListView):
     model = Post
     template_name = 'onlineshop/clothes.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
 
 
-class PostDetailView(DetailView):
+class PostDetailView(LoginRequiredMixin, DetailView):
     model = Post
     template_name = 'onlineshop/post_detail.html'
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
