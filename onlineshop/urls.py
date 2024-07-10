8fb7cd9c258f9c952e17392fe0f3e6c6d1ad40da
mashabodowskaya <mashabodovskaya@gmail.com>
2024-07-10 17:37:15 +0200

    Creating PostListView and PostDetailView for my posts functionality. Adjusting the urlpattern paths

--- onlineshop/urls.py
+++ onlineshop/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from .views import PostListView, PostDetailView
+
 
 urlpatterns = [
     path('', views.show_home, name='show_home'),
-    path('clothes/', views.clothes, name='clothes'),
     path('community/', views.about, name='community'),
-]
+    path('clothes/', PostListView.as_view(), name='clothes'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    ]
--- onlineshop/views.py
+++ onlineshop/views.py
@@ -1,8 +1,9 @@
-from time import timezone
 from django.utils import timezone
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
 
 
 def show_home(request):
@@ -13,8 +14,13 @@ def about(request):
     return render(request, 'onlineshop/community.html')
 
 
-def clothes(request):
-    context = {
-        'posts': Post.objects.all()
-    }
-    return render(request, 'onlineshop/clothes.html', context)
+class PostListView(ListView):
+    model = Post
+    template_name = 'onlineshop/clothes.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'onlineshop/post_detail.html'
