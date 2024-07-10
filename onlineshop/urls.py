7f192dbb92219ff7d87b7b25b89f6c419ff96bae
mashabodowskaya <mashabodovskaya@gmail.com>
2024-07-10 22:59:53 +0200

    Finished the CRUD project by adding the Delete method.
    Some other things to consider:
    
    1 - adding the ability of a user to add images to the post.
    
    2 - chat option
    
    3 - amend the front-end

--- onlineshop/urls.py
+++ onlineshop/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 
 urlpatterns = [
@@ -10,4 +10,5 @@ urlpatterns = [
     path('post/new', PostCreateView.as_view(), name='post_new'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
     path('post/<int:pk>/update', PostUpdateView.as_view(), name='post_update'),
+    path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post_delete'),
     ]
